6ea8af20520121b51b3a810dcdbf748756d1cadf
hotire <gngh0101@gmail.com>
2020-02-06 23:52:48 +0900

    Add retry config

---
+++ src/main/java/com/github/hotire/spring/retry/RetryConfig.java
@@ -0,0 +1,9 @@
+package com.github.hotire.spring.retry;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class RetryConfig {
+}
