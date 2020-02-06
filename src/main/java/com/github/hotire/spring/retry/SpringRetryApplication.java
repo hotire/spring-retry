aab7fef9db1483a4a7b948c70dfa39eb3b174cc4
hotire <gngh0101@gmail.com>
2020-02-06 23:40:48 +0900

    init

---
+++ src/main/java/com/github/hotire/spring/retry/SpringRetryApplication.java
@@ -0,0 +1,13 @@
+package com.github.hotire.spring.retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRetryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/hotire/spring/retry/SpringRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hotire.spring.retry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
